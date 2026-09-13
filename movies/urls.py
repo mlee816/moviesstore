2ab5281460153c3fe278fc16a42b5facbb471c24
Megan <megsl2006@gmail.com>
2026-09-13 00:08:17 -0400

    first version

---
+++ movies/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='movies.index'),
+    path('<int:id>/', views.show, name='movies.show'),
+    path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
+]
---
+++ movies/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .models import Movie, Review
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    template_data = {}
+    template_data['title'] = 'Movies'
+    template_data['movies'] = Movie.objects.all()
+    return render(request, 'movies/index.html',
+                  {'template_data': template_data})
+def show(request, id):
+    movie =  Movie.objects.get(id=id)
+    reviews = Review.objects.filter(movie=movie)
+
+    template_data = {}
+    template_data['title'] = movie.name
+    template_data['movie'] = movie
+    template_data['reviews'] = reviews
+
+    return render(request, 'movies/show.html',
+                  {'template_data': template_data})
+@login_required
+def create_review(request, id):
+    if request.method == 'POST' and request.POST['comment']!= '':
+        movie = Movie.objects.get(id=id)
+        review = Review()
+        review.comment = request.POST['comment']
+        review.movie = movie
+        review.user = request.user
+        review.save()
+        return redirect('movies.show', id=id)
+    else:
+        return redirect('movies.show', id=id)
